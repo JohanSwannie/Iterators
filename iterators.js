29b2443bcf410a07ed9e612885b62ff9b06990af
jswan <jswannieman@gmail.com>
2023-09-06 09:05:37 +1200

    Update iterators Javascript Component

--- iterators.js
+++ iterators.js
@@ -19,3 +19,20 @@ for (let value of list2) {
   pippie2.innerHTML = value;
   container2.appendChild(pippie2);
 }
+
+let list3 = [12, 36, 19, 23, 4, 18, 10, 21, 7];
+
+const iterator3 = list3[Symbol.iterator]();
+
+const container3 = document.querySelector(".list3");
+
+for (let i = 0; i < list3.length + 1; i++) {
+  const object3 = iterator3.next();
+  let pString = "";
+  for (let item in object3) {
+    pString += ` | ${item} : ${object3[item]} | `;
+  }
+  const pippie3 = document.createElement("p");
+  pippie3.innerHTML = pString;
+  container3.appendChild(pippie3);
+}
